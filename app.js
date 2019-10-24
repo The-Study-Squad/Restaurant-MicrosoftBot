9aefdd018682d9a30aef2583aebadafcea92f8d1
Mitusha <aryamitusha@gmail.com>
2019-10-24 23:42:18 +0530

    new files

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var restify = require('restify');
 var builder = require('botbuilder');
 // Lets setup the Restify Server
 var server = restify.createServer();
-server.listen(process.env.port || process.env.PORT || 53245, function () {
+server.listen(process.env.port || process.env.PORT || 3978, function () {
 console.log('%s listening to %s', server.name, server.url);
 });
 // Create chat connector for communicating with the Bot Framework Service
@@ -12,7 +12,7 @@ appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 // Listen for messages from users
 server.post('/foodiebot', connector.listen());
-// Echo their message back.. just parrotting!
+// Echo their message back.
 var bot = new builder.UniversalBot(connector, function (session) {
 session.send("You said: %s", session.message.text);
 });
