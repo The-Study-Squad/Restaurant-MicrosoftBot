43214e0e4bf721b22b5cd6045ced041c5604b426
Mitusha Arya <aryamitusha@gmail.com>
2019-10-31 17:37:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var connector = new builder.ChatConnector({
 appId: process.env.MICROSOFT_APP_ID,
 appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
+//get zomato ID and the api key 
 // Listen for messages from users
 server.post('/foodbot', connector.listen());
 // Echo their message back
