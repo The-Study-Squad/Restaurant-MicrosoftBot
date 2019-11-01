34105c214d84440b0c954c50105adcecab1489b0
Mitusha Arya <aryamitusha@gmail.com>
2019-11-01 09:42:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 // Lets setup the Restify Server
+//running multiple servers
 var server = restify.createServer();
 server.listen(process.env.port || process.env.PORT || 3978, function () {
 console.log('%s listening to %s', server.name, server.url);
