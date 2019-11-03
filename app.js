35986b78800b3954f684520972f493d36cbbb7fb
Mitusha Arya <aryamitusha@gmail.com>
2019-11-03 11:10:13 +0530

    Update app.js
    Generate new API Key

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ appId: process.env.MICROSOFT_APP_ID,
 appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 //get zomato ID and the api key 
+//Changes (Generating new API)
 // Listen for messages from users
 server.post('/foodbot', connector.listen());
 // Echo their message back
